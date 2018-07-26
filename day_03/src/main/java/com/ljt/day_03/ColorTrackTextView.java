6b044ba23f47c8fbb00aae446875d5eb24aecd20
joeljt <joseph_l@126.com>
2018-07-26 15:30:36 +0800

    实现一个文字两种颜色

--- day_03/src/main/java/com/ljt/day_03/ColorTrackTextView.java
+++ day_03/src/main/java/com/ljt/day_03/ColorTrackTextView.java
@@ -2,18 +2,27 @@ package com.ljt.day_03;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
 /**
  * Created by lijiateng on 2018/7/26.
+ *
+ * 主要技术点：
+ *  利用 canvas.clipRect() 方法，不断地去改变切割点，
+ *  然后用两个不同颜色的画笔对两侧进行绘制，从而实现一个文本两种颜色的效果
+ *
  */
 
 public class ColorTrackTextView extends android.support.v7.widget.AppCompatTextView {
 
     private Paint mOriginPaint, mChangePaint;
+
+    private float mProgress = 0.5f;
 
     public ColorTrackTextView(Context context) {
         this(context, null);
@@ -52,5 +61,46 @@ public class ColorTrackTextView extends android.support.v7.widget.AppCompatTextV
         return paint;
     }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        // 不使用 super.onDraw()，因为要自己进行绘制操作
+
+        // 根据进度把中间值算出来
+        int middle = (int) (mProgress * getWidth());
+
+        // 绘制不变色的区域
+        drawText(canvas, mOriginPaint, 0, middle);
+
+        // 绘制变色区域
+        drawText(canvas, mChangePaint, middle, getWidth());
+
+    }
+
+    private void drawText(Canvas canvas, Paint paint, int start, int end) {
+
+        // 保存当前画布状态
+        canvas.save();
+
+        Rect rect = new Rect(start, 0, end, getHeight());
+        canvas.clipRect(rect);
+
+        // 获取文字的基本宽高信息
+        String text = getText().toString();
+        Rect textBounds = new Rect();
+        paint.getTextBounds(text, 0, text.length(), textBounds);
+        Paint.FontMetricsInt metrics = paint.getFontMetricsInt();
+
+        // 获取起始位置
+        int x = getWidth() / 2 - textBounds.width() / 2;
+        int y = getHeight() / 2 + (metrics.bottom - metrics.top) / 2 - metrics.bottom;
+        canvas.drawText(text, x, y, paint);
+
+        canvas.drawText(text, x, y, paint);
+
+        // 清空画布属性，方便接下来绘制变色的部分
+        canvas.restore();
+
+    }
 
 }
