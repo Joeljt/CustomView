d6e423daac8e438829f058ba5cf38a7f555a0cb6
joeljt <joseph_l@126.com>
2018-07-29 14:55:04 +0800

    进度更新效果实现

--- day_04/src/main/java/com/ljt/day_04/CircleProgressView.java
+++ day_04/src/main/java/com/ljt/day_04/CircleProgressView.java
@@ -2,8 +2,11 @@ package com.ljt.day_04;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -16,10 +19,13 @@ public class CircleProgressView extends View {
 
     private Paint mOuterPaint, mInnerPaint, mTextPaint;
 
+    private int mStrokeWidth;
     private int mOuterCircleColor;
     private int mInnerCircleColor;
     private int mProgressTextColor;
     private int mProgressTextSize;
+
+    private float mCurrProgress = 0.0f;
 
     public CircleProgressView(Context context) {
         this(context, null);
@@ -33,6 +39,7 @@ public class CircleProgressView extends View {
         super(context, attrs, defStyleAttr);
 
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CircleProgressView);
+        mStrokeWidth = typedArray.getDimensionPixelSize(R.styleable.CircleProgressView_progress_stroke_width, 15);
         mOuterCircleColor = typedArray.getColor(R.styleable.CircleProgressView_outer_circle_color, Color.BLUE);
         mInnerCircleColor = typedArray.getColor(R.styleable.CircleProgressView_inner_circle_color, Color.RED);
         mProgressTextColor = typedArray.getColor(R.styleable.CircleProgressView_progress_text_color, Color.RED);
@@ -40,12 +47,74 @@ public class CircleProgressView extends View {
         typedArray.recycle();
 
         // 绘制外层整体进度圆形画笔
+        mOuterPaint = new Paint();
+        mOuterPaint.setAntiAlias(true);
+        mOuterPaint.setColor(mOuterCircleColor);
+        mOuterPaint.setStrokeWidth(mStrokeWidth);
+        mOuterPaint.setStyle(Paint.Style.STROKE);
 
         // 绘制进度圆形画笔
+        mInnerPaint = new Paint();
+        mInnerPaint.setAntiAlias(true);
+        mInnerPaint.setColor(mInnerCircleColor);
+        mInnerPaint.setStrokeWidth(mStrokeWidth);
+        mInnerPaint.setStyle(Paint.Style.STROKE);
 
         // 绘制文字画笔
+        mTextPaint = new Paint();
+        mTextPaint.setAntiAlias(true);
+        mTextPaint.setColor(mProgressTextColor);
+        mTextPaint.setTextSize(mProgressTextSize);
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int size = widthSize < heightSize ? widthSize : heightSize;
+
+        // 保证是一个正方形
+        setMeasuredDimension(size, size);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // 1. 画外层大圆
+
+        // 设置外切矩形
+        RectF rectF = new RectF(mStrokeWidth/2, mStrokeWidth/2, getWidth() - mStrokeWidth/2, getHeight()-mStrokeWidth/2);
+        canvas.drawArc(rectF, 0, 360, false, mOuterPaint);
+
+        // 2. 画进度圆形
+        canvas.drawArc(rectF, 0, mCurrProgress * 360, false, mInnerPaint);
+
+        // 3. 绘制进度文字
+
+        // 获取文字信息
+        String currProgressText = String.valueOf((int) (mCurrProgress * 100)) + "%";
+        Rect bounds = new Rect();
+        mTextPaint.getTextBounds(currProgressText, 0, currProgressText.length(), bounds);
+        Paint.FontMetricsInt fontMetrics = mTextPaint.getFontMetricsInt();
 
 
+        // 确定起始坐标
+        int x = getWidth() / 2 - bounds.width() / 2;
+        int y = getHeight() / 2 + (fontMetrics.bottom - fontMetrics.top) / 2 - fontMetrics.bottom;
+
+        canvas.drawText(currProgressText, x, y, mTextPaint);
+
+    }
+
+    public void setProgress(float progress) {
+        this.mCurrProgress = progress;
+        invalidate();
     }
 
 }
--- day_04/src/main/java/com/ljt/day_04/MainActivity.java
+++ day_04/src/main/java/com/ljt/day_04/MainActivity.java
@@ -1,13 +1,36 @@
 package com.ljt.day_04;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CircleProgressView cpView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cpView = (CircleProgressView)findViewById(R.id.cp_view);
+
+    }
+
+    public void test(View view) {
+
+        ValueAnimator valueAnimator = ObjectAnimator.ofFloat(0, 1);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                float value = (float) valueAnimator.getAnimatedValue();
+                cpView.setProgress(value);
+            }
+        });
+        valueAnimator.setDuration(1500);
+        valueAnimator.start();
+
     }
 }
