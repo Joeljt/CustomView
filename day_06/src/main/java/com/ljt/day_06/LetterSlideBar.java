1c2f75492aa03f6db7e7d1026876f6b8b249a437
Joeljt <joseph_l@126.com>
2018-08-01 08:27:58 +0800

    绘制26个英文字母

--- day_06/src/main/java/com/ljt/day_06/LetterSlideBar.java
+++ day_06/src/main/java/com/ljt/day_06/LetterSlideBar.java
@@ -2,6 +2,7 @@ package com.ljt.day_06;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
@@ -15,6 +16,11 @@ import android.view.View;
  */
 
 public class LetterSlideBar extends View {
+
+    // 定义26个字母
+    public static String[] mLetters = {"A", "B", "C", "D", "E", "F", "G", "H", "I",
+            "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+            "W", "X", "Y", "Z", "#"};
 
     private Paint mLetterPaint, mFocusedLetterPaint;
     private int letterColor, letterFocusedColor;
@@ -60,6 +66,26 @@ public class LetterSlideBar extends View {
         int width = (int) (getPaddingLeft() + mLetterPaint.measureText("A") + getPaddingRight());
 
         setMeasuredDimension(width, height);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        // 绘制 26 个英文字母
+        for (String letter : mLetters) {
+
+            // x 坐标的位置 -> (控件的宽度 - 字母宽度) 的一半
+            int x = (int) (getWidth() / 2 - mLetterPaint.measureText(letter) / 2);
+
+            // 基线的位置，需要根据字母数量和当前位置来不断计算得到
+            // 当前字母的基线 = 当前字母之前的所有字母的高度 + 当前字母的基线
+//            (getHeight() / mLetters.length) * ;
+            int y = 0;
+
+            canvas.drawText(letter, x, y, mLetterPaint);
+
+        }
 
     }
 }
